3e9d76a064779a9861e4e6afb38ccb0225c49b36
ntasti <ntaasti@gmail.com>
2023-10-16 18:27:11 +0400

    h

--- src/Main.java
+++ src/Main.java
@@ -3,63 +3,54 @@ import java.util.Stack;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
         //кол-во элементов стека
         System.out.println("Введите кол-во элементов стеке ");
-        int a = scanner.nextInt();
+        int size = scanner.nextInt();
 
         //создание и заполнение первого и второго стека
         Stack<Integer> stack1 = new Stack<>();
         Stack<Integer> stack2 = new Stack<>();
         System.out.println("Введите элементы первого стека");
-        stack1 = StackComp(a);
+        stack1 = stackComp(size);
         System.out.println("Введите элементы  второго  стека");
-        stack2 = StackComp(a);
+        stack2 = stackComp(size);
 
         //вывод стеков до смены информации
         System.out.println("Первый стек до смены " + stack1);
         System.out.println("Второй стек до смены " + stack2);
 
         //Трансфер данных в динамический массив
-        List<Integer> save1 = new ArrayList<>();
-        List<Integer> save2 = new ArrayList<>();
-        save1 = DataTransfer(stack1, a);
-        save2 = DataTransfer(stack2, a);
+        List<Integer> tempList1 = dataTransfer(stack1, size);
+        List<Integer> tempList2 = dataTransfer(stack2, size);
 
         //Получение результата
-        stack1 = result(save2);
-        stack2 = result(save1);
+        stack1.addAll(tempList2);
+        Collections.reverse(stack1);
+        stack2.addAll(tempList1);
+        Collections.reverse(stack2);
         System.out.println("Стек 1 после смены информации" + stack1);
         System.out.println("Стек 2 после смены информации" + stack2);
-
     }
 
     //    заполнение стека с клавиатуры
-    public static Stack<Integer> StackComp(int a) {
+    public static Stack<Integer> stackComp(int size) {
         Scanner scanner = new Scanner(System.in);
         Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < a; i++) {
-            int element = scanner.nextInt();
-            stack.push(element);
+        for (int i = 0; i < size; i++) {
+            stack.push(scanner.nextInt());
         }
         return stack;
     }
 
     //Перенос данных из стеков в динамический массивы
-    public static List<Integer> DataTransfer(Stack<Integer> stack, int a) {
-        List<Integer> save = new ArrayList<>();
-        for (int i = 0; i < a; i++)
-            save.add(stack.pop());
-        Collections.reverse(save);
-        return save;
+    public static List<Integer> dataTransfer(Stack<Integer> stack, int size) {
+        List<Integer> tempList = new ArrayList<>();
+        for (int i = 0; i < size; i++)
+            tempList.add(stack.pop());
+        return tempList;
     }
-
-    public static Stack<Integer> result(List<Integer> save) {
-        Stack<Integer> stack = new Stack<>();
-        stack.addAll(save);
-        return stack;
-    }
-
 }
